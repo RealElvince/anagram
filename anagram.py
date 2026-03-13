7e16f17d0b8da21b4a6f8681599059837cfea026
Elvince Oduor <124202145+RealElvince@users.noreply.github.com>
2026-03-13 16:46:03 +0300

    anagram code

--- anagram.py
+++ anagram.py
@@ -1,1 +1,16 @@
+def anagram():
+    first_word = input("Enter first word: ")
+    second_word = input("Enter second word: ")
 
+    first_word_lower = first_word.lower()
+    second_word_lower = second_word.lower()
+
+    if len(first_word_lower) != len(second_word_lower):
+        print(f"{first_word} and {second_word} are not anagrams!")
+    else:
+        if sorted(first_word_lower) == sorted(second_word_lower):
+            print(f"{first_word} and {second_word} are anagrams!")
+        else:
+            print(f"{first_word} and {second_word} are not anagrams!")
+
+anagram()
